394c0daccf64353a18f986a9483019bf0eab56b2
Kibreab Aklilu <Akkeba37@gmail.com>
2023-10-04 20:51:30 +0300

    Building the product model

---
+++ app/api/products/[id]/route.tsx
@@ -0,0 +1,64 @@
+import { NextRequest, NextResponse } from "next/server";
+import schema from "../schema";
+import { prisma } from "@/prisma/client";
+
+//GET a single product
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const product = await prisma.product.findUnique({
+    where: {
+      id: parseInt(params.id),
+    },
+  });
+  if (!product)
+    return NextResponse.json({ error: "Product not found" }, { status: 404 });
+
+  return NextResponse.json(product);
+}
+
+// Update a product
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const body = await request.json();
+  const validation = schema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(validation.error.errors, { status: 400 });
+
+  const product = await prisma.product.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+
+  if (!product)
+    return NextResponse.json({ error: "Product not found" }, { status: 404 });
+
+  const updatedproduct = await prisma.product.update({
+    where: { id: product.id },
+    data: {
+      name: body.name,
+      price: body.price,
+    },
+  });
+
+  return NextResponse.json(updatedproduct);
+}
+
+// Delete a product
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const product = await prisma.product.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+  if (!product)
+    return NextResponse.json({ error: "product Not Found" }, { status: 404 });
+
+  await prisma.product.delete({
+    where: { id: parseInt(params.id) },
+  });
+  return NextResponse.json({});
+}
