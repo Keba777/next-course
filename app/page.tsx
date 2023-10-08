af9e636de1d1cbdff0b32ea7d5bd83edc3b9e8f1
Kibreab Aklilu <Akkeba37@gmail.com>
2023-10-08 20:15:00 +0300

    Optimizing images

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,18 @@
 import Image from "next/image";
-import Link from "next/link";
-import ProductCard from "./components/ProductCard";
-import { getServerSession } from "next-auth";
-import { authOptions } from "./api/auth/[...nextauth]/route";
+import coffee from "@/public/images/coffee.jpg";
 
 export default async function Home() {
-  const session = await getServerSession(authOptions);
   return (
-    <main>
-      <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
-      <Link href="/users">Users</Link>
-      <ProductCard />
+    <main className="relative h-screen">
+      <Image
+        src="https://bit.ly/react-cover"
+        alt="coffee"
+        fill
+        sizes="(max-width: 480px) 100vw, (max-width: 768px) 50vw, 33vw"
+        className="object-cover"
+        quality={100}
+        priority
+      />
     </main>
   );
 }
