ec0adb88145a1660b22ca85343744ded3a4ce78c
Kibreab Aklilu <Akkeba37@gmail.com>
2023-10-08 21:28:26 +0300

    Lazy Loading

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,24 @@
-import Image from "next/image";
-import coffee from "@/public/images/coffee.jpg";
+"use client";
 
-export default async function Home() {
+
+export default function Home() {
+  
   return (
     <main className="relative h-screen">
-      <h1 className="font-poppins">Hello World</h1>
-      <Image
-        src="https://bit.ly/react-cover"
-        alt="coffee"
-        fill
-        sizes="(max-width: 480px) 100vw, (max-width: 768px) 50vw, 33vw"
-        className="object-cover"
-        quality={100}
-        priority
-      />
+      <h1>Hello World</h1>
+      <button onClick={async () => {
+        const _ = (await import("lodash")).default
+
+        const users = [
+          {name: 'c'},
+          {name: 'b'},
+          {name: 'a'},
+        ]
+        const sorted = _.orderBy(users, ["name"])
+        console.log(sorted);
+        
+      }}>Show</button>
+      
     </main>
   );
 }
